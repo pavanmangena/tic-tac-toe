7925af9401eed4e8121501e686c406f69214adf1
pavanmangena <atulkumar.verma@ltts.com>
2020-01-06 17:33:35 +0530

    created tictactoe game

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import './App.css'
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+class Board extends React.Component {
+  constructor()
+  {
+    super();
+    this.state = this.firstState();
+  }
+  firstState() {
+    return {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    };
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  resetAll()  {
+    this.setState(this.firstState())
+  }
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'WINNER is ' + winner;
+    } else {
+      status = 'Current player is ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    return (
+      <div className="title-style">
+        <h1>TIC TAC TOE Game</h1>
+        <div className="marquee-style">
+            <marquee>
+              Move once made, cannot be undo
+            </marquee>
+          </div>
+        <div className="board-style">
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+          <div className="button-class">
+          <button className="button-class-style" onClick={() => this.resetAll()}> Reset </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+class App extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+      </div>
+    );
+  }
+}
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+export default App;
